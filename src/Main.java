6a29cc69dc2007a299da2961ccb4c1c692cfa56a
ILYA_MOLCHANOV <ilyamolchanov648@yandex.ru>
2023-08-17 11:12:17 +0300

    Made MyArrayList

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyArrayList<Integer> list = new MyArrayList<>();
+
+        // Добавление элементов
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // Вставка элемента по индексу
+        list.add(1, 4);
+
+        // Получение элемента по индексу
+        System.out.println(list.get(2));
+
+        // Удаление элемента
+        list.remove(2);
+
+        // Проверка размера
+        System.out.println(list.size());
+
+        // Очистка списка
+        list.clear();
+        System.out.println(list.size());
+
+    }
+
+}
+
---
+++ src/MyArrayList.java
@@ -0,0 +1,56 @@
+class MyArrayList<T> {
+
+    private T[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = (T[])new Object[10];
+    }
+
+    public void add(T item) {
+        ensureCapacity(size + 1);
+        elements[size++] = item;
+    }
+
+    public void add(int index, T item) {
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    public T get(int index) {
+        checkBounds(index);
+        return elements[index];
+    }
+
+    public void remove(int index) {
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+    }
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        size = 0;
+        elements = (T[])new Object[10];
+    }
+
+    private void ensureCapacity(int capacity) {
+        if (capacity > elements.length) {
+            T[] newElements = (T[])new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+    }
+
+    private void checkBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    // реализация остальных методов
+
+    // вспомогательные методы
+}
